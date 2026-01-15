af77c07ae057a99458c9964d02d1912ecb2c5053
ugonna <anyugonna@gmail.com>
2026-01-14 23:57:51 -0600

    Set up initial project files and create functions for basic math calculations

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Calculates the sum of two numbers by adding them together
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns {number}
+ */
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+/**
+ * Calculates the difference of two numbers by subtracting num2 from num1
+ * @param {*} num1 minuend - starting number
+ * @param {*} num2 subtrahend - number taken away
+ * @returns {number}
+ */
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+/**
+ * Calculates the product of two numbers by multiplying them together
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns {number} 
+ */
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+/**
+ * Calculates the quotient of two numbers by dividing num1 by num2
+ * @param {*} num1 dividend - number being divided
+ * @param {*} num2 divisor - number dividing by
+ * @returns {number}
+ */
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+
